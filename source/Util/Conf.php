9ce85d03e6a695f4d5f87d0a5cdc3d9fc6b524aa
Oséias Gomes <47332200+sdgomes@users.noreply.github.com>
2021-04-06 16:02:59 -0300

    Add files via upload

--- source/Util/Conf.php
+++ source/Util/Conf.php
@@ -1,1 +1,14 @@
+<?php
+class Conf
+{
 
+    private static $isProducao = false;
+
+    public static function urlBanco()
+    {
+        if (self::$isProducao)
+            return array("mysql:host=108.179.193.15:3306;dbname=xnora386_prod_urakami", "xnora386_devs", "SCP-DEVS2021");
+        else
+            return array("mysql:host=localhost;dbname=appcargo_medias", "root", "");
+    }
+}
